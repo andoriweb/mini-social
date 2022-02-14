bec86842266ce0ee6caa5436858f2a60c4d26827
andoriweb <andoriweb@gmail.com>
2022-02-14 11:54:45 +0200

    HTML template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid">
+        <header className="header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Mini Social</h1>
+        </header>
+        <aside className="sidebar">
+          <nav>
+            <ul>
+              <li>item-1</li>
+              <li>item-2</li>
+              <li>item-3</li>
+              <li>item-4</li>
+              <li>item-5</li>
+            </ul>
+          </nav>
+        </aside>
+        <div className="content"></div>
+        <footer className="footer"></footer>
+      </div>
     </div>
   );
 }
